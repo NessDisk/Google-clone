75531bb8f332245aaa49cb6e900808b05a56e2ca
NessDisk <ness.cero1@gmail.com>
2022-07-10 22:05:01 -0500

    Helper

--- google-cl/src/useGoogleSearch.js
+++ google-cl/src/useGoogleSearch.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import API_KEY from './Keys';
 
 const CONTEXT_KEY ="6e4014a5cec234392"
-
+//https://developers.google.com/custom-search/v1/using_rest 
+//cuando se desactualiza ir a reactivar la apikey
  const useGoogleSearch = (term) => {
   
 
